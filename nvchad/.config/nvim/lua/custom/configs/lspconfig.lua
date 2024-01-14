433206155d2f9fd46d1ff4ef273042799b596e96
Bernt Anker <bernt.anker@me.com>
2024-01-14 10:23:53 +0100

    set css linting for unknownAtRules to ignore

--- nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvchad/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -21,7 +21,24 @@ lspconfig.eslint.setup({
 
 lspconfig.cssls.setup({
   on_attach = on_attach,
-  capabilities = capabilities
+  capabilities = capabilities,
+  settings = {
+  css = { validate = true,
+        lint = {
+            unknownAtRules = "ignore"
+        }
+  },
+  scss = { validate = true,
+        lint = {
+            unknownAtRules = "ignore"
+        }
+  },
+  less = { validate = true,
+        lint = {
+            unknownAtRules = "ignore"
+        }
+  },
+ },
 })
 
 vim.diagnostic.config({
